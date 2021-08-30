f96d0a86e4af1206858cc6e9d906f2a03327f035
Daniel010101010101 <88622971+Daniel010101010101@users.noreply.github.com>
2021-08-30 15:38:14 -0600

    Entidades

---
+++ LabSegundaParte/LabSegundaParte/Entidades/Persona.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSegundaParte.Entidades
+{
+    class Persona
+    {
+
+        private String nombre;
+        private int edad;
+        private String sexo;
+        private double peso;
+        private double altura;
+
+     
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public string Sexo { get => sexo; set => sexo = value; }
+        public double Peso { get => peso; set => peso = value; }
+        public double Altura { get => altura; set => altura = value; }
+
+        public Persona() { }
+
+        public Persona(string nombre, int edad, string sexo)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.sexo = sexo;
+        }
+
+        public Persona(string nombre, int edad, string sexo, double peso, double altura)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.sexo = sexo;
+            this.peso = peso;
+            this.altura = altura;
+        }
+
+        public Persona(double peso, double altura)
+        {
+            this.peso = peso;
+            this.altura = altura;
+        }
+        public override string ToString()
+        {
+            return "Nota primer periodo: " + periodo1 +
+                "\nNota segundo periodo: " + periodo2 +
+                "\nNota tercer periodo: " + periodo3 +
+                "\nResultado: " + nota;
+        }
+    }
+}
