a0417a4304abd28f1cd14a9cae3b208697529673
FranSolorzano <87993086+FranSolorzano@users.noreply.github.com>
2021-08-30 16:12:33 -0600

    actualizacion

---
+++ LabSegundaParte/LabSegundaParte/Negocio/ClsPersona.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LabSegundaParte;
+
+namespace LabSegundaParte.Negocio
+{
+    class ClsPersona
+    {
+
+        public double persona = new persona.Peso(); 
+
+
+
+        if (double pro = persona.Peso / Math.Pow(persona.Altura, 2))
+            {
+               
+                return null;
+            }
+            else
+            {
+                
+                producto = datos.buscarProductoPorID(producto);
+
+                
+                if (producto == null)
+                {
+                    return null;
+                }
+                
+                else
+
+    }
+}
